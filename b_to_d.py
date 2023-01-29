3cc2da4c7b62546341ca9cb85f0bd255f0567076
nuaimMansuri <123896306+nuaimMansuri@users.noreply.github.com>
2023-01-29 23:52:11 +0530

    Create b_to_d.py

---
+++ b_to_d.py
@@ -0,0 +1,12 @@
+print("\nEnter the Binary Number: ")
+bnum = int(input())
+
+dnum = 0
+i = 1
+while bnum!=0:
+    rem = bnum%10
+    dnum = dnum + (rem*i)
+    i = i*2
+    bnum = int(bnum/10)
+
+print("\n Decimal Value is = ", dnum)
